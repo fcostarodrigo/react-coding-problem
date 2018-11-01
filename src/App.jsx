9cb5c1061838ea10c0c77f9a7ed2a43a92eef1b8
Rodrigo <fcostarodrigo@yahoo.com>
2018-10-31 21:54:54 -0300

    Update layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
 import React from "react";
+import { Header, Icon, Segment, Container } from "semantic-ui-react";
 import IssueTable from "./issueTable/IssueTable";
 
-export default () => <IssueTable />;
+export default () => (
+  <React.Fragment>
+    <Header as="h1" icon textAlign="center">
+      <Icon name="react" circular />
+      <Header.Content>React issue status from GitHub</Header.Content>
+    </Header>
+    <Container>
+      <Segment raised>
+        <IssueTable />
+      </Segment>
+    </Container>
+  </React.Fragment>
+);
